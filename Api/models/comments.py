021a97d2243124d1b6b20161d8d597e204043c36
JoyLubega <jollankent@gmail.com>
2019-06-20 16:46:40 +0300

    Get recipes

--- Api/models/comments.py
+++ Api/models/comments.py
@@ -2,21 +2,23 @@ from datetime import datetime
 
 from Api.api import db
 
-class CommentsModel(db.Model):
+class ReviewsModel(db.Model):
     """
-    Comments Database Model
+    Review Database Model includes comments and rates
     """
-    __tablename__ = 'comments'
+    __tablename__ = 'reviews'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     date_added = db.Column(db.DateTime, default=datetime.utcnow())
     date_updated = db.Column(db.DateTime, default=datetime.utcnow())
     recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'))
     comment_text = db.Column(db.String(100))
+    rate = db.Column(db.Integer, nullable=False)
     
 
-    def __init__(self, comment_text, recipe_id):
+    def __init__(self, comment_text, recipe_id, rate):
         self.comment_text = comment_text
         self.recipe_id = recipe_id
+        self.rate = rate
 
     def save(self):
         """
